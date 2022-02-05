383890d7c625cf50ccb95ce0a4356924888283ee
yutsi <darthyutsi1@gmail.com>
2022-02-05 10:51:02 -0500

    commit

--- client/src/components/Person.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Person = ({ persons, remove }) => {
-  return (
-    <li>{persons.name} {persons.number} <button onClick={remove}>Delete</button></li>
-  )
-}
-
-export default Person
