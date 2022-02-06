9acc6499e9a3e2a026c64c0dd4e0174b5917222c
yutsi <darthyutsi1@gmail.com>
2022-02-06 11:27:48 -0500

    base-url change for heroku

--- client/src/services/persons.js
+++ client/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.BASE_URL
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3002/api/persons'
 
 const getAll = () => {
   const request = axios.get(BASE_URL)
